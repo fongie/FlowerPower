aab66a5cf3f3a4a434820dc15c6022ed7c940def
Max Körlinge <mkorlinge@hotmail.com>
2018-04-17 12:40:14 +0200

    working (and tested) Plant class that can be run in threads and provide different values

--- src/raspberry/Webserver/externals/externalmodel/plant.py
+++ src/raspberry/Webserver/externals/externalmodel/plant.py
@@ -1,27 +1,37 @@
-import threading
+import threading, time
 
 '''
+The Plant class symbolizes a Plant in the system. It handles all communication with the external hardware connected with the plant, such as the moist-sensoring Arduino, and the Tellstick Duo for irrigation.
+
+The class is meant to run as a separate Thread. It loops forever until you call set the variable runSignal to False on the thread, then it stops.
+
 https://stackoverflow.com/questions/19033818/how-to-call-a-function-on-a-running-python-thread
 
-men ska vi verkligen köra en thread hela tiden?? kanke bättre att först åtminstone låta externalcontroller spawna trådar som kollar alla blommor vid behov i stället
+TODO: prevent race condition
 '''
-
 class Plant(threading.Thread):
 
-    def __init__(self, plantID):
+    def __init__(self, *args):
         threading.Thread.__init__(self)
-        self.lastMoistReading = 0
-        self.plantID = plantID
+        if len(args) == 1:
+            self.lastMoistReading = 0
+            self.plantID = args[0]
+            self.runSignal = True
+        elif len(args) == 2: # for testing purposes. provides a hardcoded moistnessvalue
+            self.lastMoistReading = args[1]
+            self.plantID = args[0]
+            self.runSignal = True
 
     def run(self):
-        while True:
-            pass
+        while self.runSignal:
+            self.updateMinDryness()
+            time.sleep(2) # how often we poll the Arduino for moistness, in seconds
 
     def getMoistness(self):
         return self.lastMoistReading
 
-    def abortWatering():
+    def abortWatering(self):
         pass
 
-    def updateMinDryness():
+    def updateMinDryness(self):
         pass
--- src/raspberry/Webserver/externals/externalmodel/plant_test.py
+++ src/raspberry/Webserver/externals/externalmodel/plant_test.py
@@ -1,6 +1,9 @@
-import pytest, threading
+import pytest, threading, time
 from .plant import Plant
 
+def test_getMoistness():
+    p = Plant(1, 13)
+    assert p.getMoistness() == 13
 
 def test_startAsThread():
     thread1 = Plant(1)
@@ -9,10 +12,24 @@ def test_startAsThread():
     thread1.start()
     thread2.start()
 
+    time.sleep(2)
     # main thread + 2 new threads = 3 that should run
     assert threading.active_count() == 3
+    time.sleep(1)
+    thread1.runSignal = False
+    thread2.runSignal = False
 
+def test_getDifferentMoistnessFromThreads():
+    p1 = Plant(1, 10)
+    p2 = Plant(2, 25)
 
-def test_getMoistness():
-    p = Plant(1)
-    assert p.getMoistness() == 0
+    p1.start()
+    p2.start()
+
+    time.sleep(1)
+    assert p1.getMoistness() == 10
+    assert p2.getMoistness() == 25
+    time.sleep(1)
+
+    p1.runSignal = False
+    p2.runSignal = False
