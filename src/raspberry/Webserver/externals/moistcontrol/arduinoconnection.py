ffb9b53fa24bc1e6cf51970ccdb64f4e6c60422a
Erik <Erik@n170-p68.eduroam.kth.se>
2018-04-18 11:40:47 +0200

    request lib

--- src/raspberry/Webserver/externals/moistcontrol/arduinoconnection.py
+++ src/raspberry/Webserver/externals/moistcontrol/arduinoconnection.py
@@ -1,11 +1,12 @@
-from network import WLAN
+#from network import WLAN
 import urllib2
 import json
+import requests
 
 class ArduinoConnection:
-	def __init__(self):
-		wlan = WLAN(mode=WLAN.STA)
-		wlan.connect("Isabels iPhone", auth=(net.sec, "ifpejnn09q53y"), timeout=5000)
+    #def __init__(self):
+    #wlan = WLAN(mode=WLAN.STA)
+    #wlan.connect("Isabels iPhone", auth=(net.sec, "ifpejnn09q53y"), timeout=5000)
 
 	def getValue(self):
 		r = requests.get('https://172.20.10.3/getValues.json')
