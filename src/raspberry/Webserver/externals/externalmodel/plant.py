5720e7ca948d9beec753e390333162a68fbdd8e0
Max Körlinge <mkorlinge@hotmail.com>
2018-04-18 09:23:38 +0200

    added documentation on plant class

--- src/raspberry/Webserver/externals/externalmodel/plant.py
+++ src/raspberry/Webserver/externals/externalmodel/plant.py
@@ -9,8 +9,7 @@ The class is meant to run as a separate Thread. It loops forever until you call 
 '''
 class Plant(threading.Thread):
 
-# (self, PlantID, ?moistness)
-
+    ''' Constructor for Plant. Params: plantID, an int identifier. '''
     def __init__(self, plantID):
         threading.Thread.__init__(self)
 
@@ -19,17 +18,20 @@ class Plant(threading.Thread):
         self.arduinoConnection = ArduinoConnection()
         self.runSignal = True
 
+    ''' When started as a thread, loops forever until runSignal is set to False. '''
     def run(self):
         while self.runSignal:
             self.updateMinDryness()
             time.sleep(1) # how often we poll the Arduino for moistness, in seconds
             # time.sleep(60) # production value
 
+    ''' Get the last moist reading from the Arduino connection to this plant '''
     def getMoistness(self):
         return self.lastMoistReading
 
     def abortWatering(self):
         pass
 
+    ''' Update with a new moist reading from the Arduino '''
     def updateMinDryness(self):
         self.lastMoistReading = self.arduinoConnection.readValue()
