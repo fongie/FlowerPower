66eb545ad1291f4c1e3e34261859692a39c2d85d
Raspben <hello@hello.com>
2018-04-18 09:53:38 +0000

    can get html from arduino on raspb

--- src/raspberry/Webserver/externals/moistcontrol/arduinoconnection.py
+++ src/raspberry/Webserver/externals/moistcontrol/arduinoconnection.py
@@ -8,6 +8,7 @@ class ArduinoConnection:
     #wlan.connect("Isabels iPhone", auth=(net.sec, "ifpejnn09q53y"), timeout=5000)
 
 	def getValue(self):
-		r = requests.get('https://172.20.10.3/getValues.json')
+		# r = requests.get('http://172.20.10.3/getValues.json')
+		r = requests.get('http://172.20.10.3/')
 		print(r.text)
 
