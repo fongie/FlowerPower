8d6a1da3d86717ace6ab2a1586487fec6cec2fb3
Erik <Erik@Eriks-MacBook-Pro.local>
2018-04-18 11:56:07 +0200

    arduino

---
+++ src/arduino/flowerpower/flowerpowerwifi.ino
@@ -0,0 +1,133 @@
+#include <SoftwareSerial.h>
+#include "WiFiEsp.h"
+int sensorPin = A0; 
+int sensorValue = 0; 
+int sensorVCC = 13;
+
+// Create WiFi module object on GPIO pin 6 (RX) and 7 (TX)
+SoftwareSerial Serial1(6, 7);
+
+// Declare and initialise global arrays for WiFi settings
+char ssid[] = "Isabels iPhone";
+char pass[] = "ifpejnn09q53y";
+
+// Declare and initialise variable for radio status 
+int status = WL_IDLE_STATUS;
+
+// Create a web server on port 80
+WiFiEspServer server(80);
+
+void setup() {
+
+   pinMode(sensorVCC, OUTPUT);
+   digitalWrite(sensorVCC, LOW);
+        
+  // Initialize serial for debugging
+  Serial.begin(115200);
+  
+  // Initialize serial for ESP module
+  Serial1.begin(9600);
+  
+  // Initialize ESP module
+  WiFi.init(&Serial1);
+
+  // Check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // Don't continue
+    while (true);
+  }
+  
+  // Attempt to connect to WiFi network
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+
+  // Start the web server
+  server.begin();
+}
+
+void loop() {
+  WiFiEspClient client = server.available();
+
+  if (client) {
+    Serial.println("New client");
+    
+    // An HTTP request ends with a blank line
+    boolean currentLineIsBlank = true;
+    
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+
+        // If you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          Serial.println("Sending response");
+
+          digitalWrite(sensorVCC, HIGH); 
+          sensorValue = analogRead(sensorPin);
+          digitalWrite(sensorVCC, LOW);
+          Serial.println(sensorValue);
+          String stringSensorValue = String(sensorValue);
+          
+          // Send a standard HTTP response header
+          client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"
+            "\r\n");
+          client.print("<!DOCTYPE HTML>\r\n");
+          client.print("<html>\r\n");
+          client.print("<head>\r\n");
+          client.print("<title>Sensor Value</title>\r\n");
+          client.print("</head>\r\n");
+          client.print("<body>\r\n");
+          client.print("<h1>Hello World!</h1>\r\n");
+          client.print("<p>"+ stringSensorValue + "</p>\r\n");
+          client.print("</body>\r\n");
+          client.print("</html>\r\n");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        }
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    
+    // Give the web browser time to receive the data
+    delay(10);
+
+    // Close the connection:
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+}
+
+
+void printWifiStatus() {
+  
+  // Print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // Print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
+
