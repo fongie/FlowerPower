006c7f599787412e01a32985b379f6dd3cf4fd8e
Erik <Erik@n170-p68.eduroam.kth.se>
2018-04-18 11:15:43 +0200

    ArduinoConnection JSon

---
+++ src/raspberry/Webserver/externals/moistcontrol/arduinoconnection.py
@@ -0,0 +1,16 @@
+from network import WLAN
+import urllib2
+import json
+
+class ArduinoConnection:
+	def __init__(self):
+		wlan.connect("Isabels iPhone", auth=(net.sec, "ifpejnn09q53y"), timeout=5000)
+
+	def getValue(self):
+		r = requests.get('https://172.20.10.3/getValues.json')
+		opener = urllib2.build_opener()
+		f = opener.open(req)
+		json = json.loads(f.read())
+		print json
+		s = json['value']
+		print int(float(s))
