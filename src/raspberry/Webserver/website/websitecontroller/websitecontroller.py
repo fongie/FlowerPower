e235a8cca13632a6d26f4b09926fcca1d93848aa
Raspben <hello@hello.com>
2018-04-19 09:27:34 +0000

    website now displays hardcoded value all way from arduinoconnection class

--- src/raspberry/Webserver/website/websitecontroller/websitecontroller.py
+++ src/raspberry/Webserver/website/websitecontroller/websitecontroller.py
@@ -3,11 +3,12 @@ from raspberry.Webserver.externals.externalscontroller.externalscontroller impor
 class WebsiteController:
     def __init__(self):
         pass
-        
+
     def getPlants(self):
         #testdata = ["123", "456", "789"]
         #testd = f1.fuktvarde()
-        ec = ExternalsController()
-        testd = ec.readPlantStatus()
+        ec = ExternalsController.getInstance()
+        #ec = ExternalsController()
+        testd = ec.readPlantStatus(1)
         return testd
         #return 'Hej FlowerPower, nu är vi igång!'
