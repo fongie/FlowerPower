d74d0e3163d296a1e96209252c9934eda2c09d96
Raspben <hello@hello.com>
2018-04-19 10:07:10 +0000

    first working version

--- src/raspberry/Webserver/externals/moistcontrol/arduinoconnection.py
+++ src/raspberry/Webserver/externals/moistcontrol/arduinoconnection.py
@@ -5,6 +5,6 @@ class ArduinoConnection:
     #wlan = WLAN(mode=WLAN.STA)
     #wlan.connect("Isabels iPhone", auth=(net.sec, "ifpejnn09q53y"), timeout=5000)
 
-        def getValue(self):
+        def readValue(self):
             r = requests.get('http://192.168.43.160/')
             return int(r.text)
